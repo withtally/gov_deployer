35103c9ecbab6e20968ab28a0cdfea6ec2d08872
Arthur Abeilice <afa7789@gmail.com>
2022-08-02 14:13:58 -0300

    removing uneeded data of config.

--- hardhat.config.js
+++ hardhat.config.js
@@ -51,16 +51,6 @@ const ARBITRUM_RIN_URL = process.env.ARBITRUM_RIN_URL
 // Etherscan key
 const ETHERSCAN_KEY = process.env.ETHERSCAN_API_KEY
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
